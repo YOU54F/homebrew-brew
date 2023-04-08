71f3c0306a6f4e92d9557aecce800dd814f2bac6
Yousaf Nabi <you@saf.dev>
2023-04-08 23:23:23 +0100

    feat: dl pact_verifier_cli / pact_mock_server_cli

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,15 +1,32 @@
 class PactRubyStandalone < Formula
   desc "Standalone pact CLI executable using the Ruby Pact impl and Travelling Ruby"
   homepage "https://github.com/you54f/pact-ruby-standalone"
+  version "3.1.0"
   
   on_macos do
     on_arm do
       url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-osx-arm64.tar.gz"
       sha256 "26675141503abcaf5eb7ed504abd6e3e165631bc2762a315ce54d9b7124a0ff6"
+      resource "pact_verifier_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-osx-aarch64.gz"
+          sha256 "e6651e4dab1ad381d9f34fa3a9f12c7756b1bf778d864ab0893fa6078b2a98e8"
+      end
+      resource "pact_mock_server_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_mock_server_cli-v1.0.0/pact_mock_server_cli-osx-x86_64.gz"
+          sha256 "4f29dc624dd1b4862170c029e59ba648dcf9cb9d306ca79279b3897d7e0faf56"
+      end
     end
     on_intel do
       url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-osx-x86_64.tar.gz"
       sha256 "ba8f2f1504a261b7e6c0879bea3926b67102942efc982dec8255e0cf93f42b94"
+      resource "pact_verifier_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-osx-x86_64.gz"
+          sha256 "042ba537784472a65722d5b1301f4d1d61486d7220fbae2691812c44f58350dc"
+      end
+      resource "pact_mock_server_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_mock_server_cli-v1.0.0/pact_mock_server_cli-osx-x86_64.gz"
+          sha256 "4f29dc624dd1b4862170c029e59ba648dcf9cb9d306ca79279b3897d7e0faf56"
+      end
     end
   end
   
@@ -17,18 +34,39 @@ class PactRubyStandalone < Formula
     on_arm do
       url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-linux-arm64.tar.gz"
       sha256 "41b271ca318f17ba90e6014d54226021526066e43ddad5e270e31522ef200720"
+      resource "pact_verifier_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-linux-aarch64.gz"
+          sha256 "48a5d6bbc5471764e77027eaf42f62f812849b083e267be2cb8ed9d36cc862bf"
+      end
+      resource "pact_mock_server_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_mock_server_cli-v1.0.0/pact_mock_server_cli-linux-aarch64.gz"
+          sha256 "3445c2109ec1fc7a58d012c5a851f67203a6b19e3ef7f21d09fdac6b3d140f30"
+      end
     end
     on_intel do
       url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-linux-x86_64.tar.gz"
       sha256 "94befa09a693527c89b357fad0328d088eb076a72b28ba97ab45d05d7c24980d"
+      resource "pact_verifier_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-linux-x86_64.gz"
+          sha256 "48a5d6bbc5471764e77027eaf42f62f812849b083e267be2cb8ed9d36cc862bf"
+      end
+      resource "pact_mock_server_cli" do
+          url "https://github.com/pact-foundation/pact-reference/releases/download/pact_mock_server_cli-v1.0.0/pact_mock_server_cli-linux-x86_64.gz"
+          sha256 "f07fe85eab349d9d8f6f60faffb8d4af02d8d0bbe8e7a756b6b5e136a837cd92"
+      end
     end
   end
 
   def install
+    # pact-ruby-standalone
     bin.install Dir["bin/*"]
     lib.install Dir["lib/*"]
-
     puts "# Run 'pact-mock-service --help' (see https://github.com/you54f/pact-ruby-standalone/releases/)"
+
+    # pact-reference
+    resource("pact_verifier_cli").stage { bin.install Dir["*"]; puts "# Run 'pact_verifier_cli --help'" }
+    resource("pact_mock_server_cli").stage { bin.install Dir["*"]; puts "# Run 'pact_mock_server_cli --help'" }
+
   end
 
   test do
--- scripts/update_tap_version.sh
+++ scripts/update_tap_version.sh
@@ -2,7 +2,10 @@
 set -e
 
 homepage="https://github.com/you54f/pact-ruby-standalone"
+homepage_pact_reference="https://github.com/pact-foundation/pact-reference"
 version=$1
+version_pact_verifier_cli=0.10.5
+version_pact_mock_server_cli=1.0.0
 FORMULAE_FILE="pact-ruby-standalone.rb"
 
 write_homebrew_formulae() {
@@ -16,15 +19,32 @@ write_homebrew_formulae() {
         echo "class PactRubyStandalone < Formula" >&3
         echo "  desc \"Standalone pact CLI executable using the Ruby Pact impl and Travelling Ruby\"" >&3
         echo "  homepage \"$homepage\"" >&3
+        echo "  version \"$version\"" >&3
         echo "  " >&3
         echo "  on_macos do" >&3
         echo "    on_arm do" >&3
         echo "      url \"$homepage/releases/download/v$version/pact-$version-osx-arm64.tar.gz\"" >&3
         echo "      sha256 \"${sha_osx_arm64}\"" >&3
+        echo "      resource \"pact_verifier_cli\" do" >&3
+        echo "          url \"$homepage_pact_reference/releases/download/pact_verifier_cli-v$version_pact_verifier_cli/pact_verifier_cli-osx-aarch64.gz\"" >&3
+        echo "          sha256 \"${sha_osx_arm64_pact_verifier_cli}\"" >&3
+        echo "      end" >&3
+        echo "      resource \"pact_mock_server_cli\" do" >&3 # No OSX arm version yet
+        echo "          url \"$homepage_pact_reference/releases/download/pact_mock_server_cli-v$version_pact_mock_server_cli/pact_mock_server_cli-osx-x86_64.gz\"" >&3
+        echo "          sha256 \"${sha_osx_x86_64_pact_mock_server_cli}\"" >&3
+        echo "      end" >&3
         echo "    end" >&3
         echo "    on_intel do" >&3
         echo "      url \"$homepage/releases/download/v$version/pact-$version-osx-x86_64.tar.gz\"" >&3
         echo "      sha256 \"${sha_osx_x86_64}\"" >&3
+        echo "      resource \"pact_verifier_cli\" do" >&3
+        echo "          url \"$homepage_pact_reference/releases/download/pact_verifier_cli-v$version_pact_verifier_cli/pact_verifier_cli-osx-x86_64.gz\"" >&3
+        echo "          sha256 \"${sha_osx_x86_64_pact_verifier_cli}\"" >&3
+        echo "      end" >&3
+        echo "      resource \"pact_mock_server_cli\" do" >&3
+        echo "          url \"$homepage_pact_reference/releases/download/pact_mock_server_cli-v$version_pact_mock_server_cli/pact_mock_server_cli-osx-x86_64.gz\"" >&3
+        echo "          sha256 \"${sha_osx_x86_64_pact_mock_server_cli}\"" >&3
+        echo "      end" >&3
         echo "    end" >&3
         echo "  end" >&3
         echo "  " >&3
@@ -32,18 +52,39 @@ write_homebrew_formulae() {
         echo "    on_arm do" >&3
         echo "      url \"$homepage/releases/download/v$version/pact-$version-linux-arm64.tar.gz\"" >&3
         echo "      sha256 \"${sha_linux_arm64}\"" >&3
+        echo "      resource \"pact_verifier_cli\" do" >&3
+        echo "          url \"$homepage_pact_reference/releases/download/pact_verifier_cli-v$version_pact_verifier_cli/pact_verifier_cli-linux-aarch64.gz\"" >&3
+        echo "          sha256 \"${sha_linux_arm64_pact_verifier_cli}\"" >&3
+        echo "      end" >&3
+        echo "      resource \"pact_mock_server_cli\" do" >&3
+        echo "          url \"$homepage_pact_reference/releases/download/pact_mock_server_cli-v$version_pact_mock_server_cli/pact_mock_server_cli-linux-aarch64.gz\"" >&3
+        echo "          sha256 \"${sha_linux_arm64_pact_mock_server_cli}\"" >&3
+        echo "      end" >&3
         echo "    end" >&3
         echo "    on_intel do" >&3
         echo "      url \"$homepage/releases/download/v$version/pact-$version-linux-x86_64.tar.gz\"" >&3
         echo "      sha256 \"${sha_linux_x86_64}\"" >&3
+        echo "      resource \"pact_verifier_cli\" do" >&3
+        echo "          url \"$homepage_pact_reference/releases/download/pact_verifier_cli-v$version_pact_verifier_cli/pact_verifier_cli-linux-x86_64.gz\"" >&3
+        echo "          sha256 \"${sha_linux_arm64_pact_verifier_cli}\"" >&3
+        echo "      end" >&3
+        echo "      resource \"pact_mock_server_cli\" do" >&3
+        echo "          url \"$homepage_pact_reference/releases/download/pact_mock_server_cli-v$version_pact_mock_server_cli/pact_mock_server_cli-linux-x86_64.gz\"" >&3
+        echo "          sha256 \"${sha_linux_x86_64_pact_mock_server_cli}\"" >&3
+        echo "      end" >&3
         echo "    end" >&3
         echo "  end" >&3
         echo "" >&3
         echo "  def install" >&3
+        echo "    # pact-ruby-standalone" >&3
         echo "    bin.install Dir[\"bin/*\"]" >&3
         echo "    lib.install Dir[\"lib/*\"]" >&3
-        echo "" >&3
         echo "    puts \"# Run 'pact-mock-service --help' (see $homepage/releases/)\"" >&3
+        echo "" >&3
+        echo "    # pact-reference" >&3
+        echo "    resource(\"pact_verifier_cli\").stage { bin.install Dir[\"*\"]; puts \"# Run 'pact_verifier_cli --help'\" }" >&3
+        echo "    resource(\"pact_mock_server_cli\").stage { bin.install Dir[\"*\"]; puts \"# Run 'pact_mock_server_cli --help'\" }" >&3
+        echo "" >&3
         echo "  end" >&3
         echo "" >&3
         echo "  test do" >&3
@@ -113,6 +154,90 @@ done
     echo "sha_osx_x86_64:" $sha_osx_x86_64
     echo "sha_linux_arm64:" $sha_linux_arm64
     echo "sha_linux_x86_64:" $sha_linux_x86_64
+
+shas_pact_verifier_cli=()
+for platform in osx linux; do 
+    for arch in aarch64 x86_64; do 
+
+        echo "⬇️  Downloading $version_pact_verifier_cli pact_verifier_cli-${platform}-${arch}.tar.gz from $homepage_pact_reference"
+        curl -LO $homepage_pact_reference/releases/download/pact_verifier_cli-v$version_pact_verifier_cli/pact_verifier_cli-${platform}-${arch}.gz
+
+        brewshasignature=( $(eval "openssl dgst -sha256 pact_verifier_cli-${platform}-${arch}.gz") )
+        echo "🔏 Checksum SHA256:\t ${brewshasignature[1]} for ${arch}"
+
+        echo "⬇️  Downloading $version_pact_verifier_cli pact_verifier_cli-${platform}-${arch}.gz.sha256 for ${platform}-${arch}"
+        echo "curl -LO $homepage_pact_reference/releases/download/pact_verifier_cli-v$version_pact_verifier_cli/pact_verifier_cli-${platform}-${arch}.gz.sha256"
+        curl -LO $homepage_pact_reference/releases/download/pact_verifier_cli-v$version_pact_verifier_cli/pact_verifier_cli-${platform}-${arch}.gz.sha256
+
+        expectedsha=( $(eval "cat pact_verifier_cli-${platform}-${arch}.gz.sha256") )
+        echo "🔏 Expected SHA1:\t ${expectedsha[0]} for ${platform}-${arch}"
+
+        if [ "${brewshasignature[1]}" == "${expectedsha[0]}" ]; then
+            echo "👮‍♀️ SHA Check: 👍 for ${arch}"
+        else
+            echo "👮‍♀️ SHA Check: 🚨 - checksums do not match! for ${arch}"
+            # exit 1
+        fi
+
+        echo "🧹 Cleaning up..."
+        rm pact_verifier_cli-${platform}-${arch}.gz
+        rm pact_verifier_cli-${platform}-${arch}.gz.sha256
+        echo "🔏 Checksum SHA256:\t ${brewshasignature[1]} for ${platform}-${arch}"
+        echo "🧪 Writing formulae..."
+        shas_pact_verifier_cli+=(${brewshasignature[1]})
+    done 
+done 
+
+    sha_osx_arm64_pact_verifier_cli=${shas_pact_verifier_cli[0]}
+    sha_osx_x86_64_pact_verifier_cli=${shas_pact_verifier_cli[1]}
+    sha_linux_arm64_pact_verifier_cli=${shas_pact_verifier_cli[2]}
+    sha_linux_x86_64_pact_verifier_cli=${shas_pact_verifier_cli[3]}
+    echo "sha_osx_arm64_pact_verifier_cli:" $sha_osx_arm64_pact_verifier_cli
+    echo "sha_osx_x86_64_pact_verifier_cli:" $sha_osx_x86_64_pact_verifier_cli
+    echo "sha_linux_arm64_pact_verifier_cli:" $sha_linux_arm64_pact_verifier_cli
+    echo "sha_linux_x86_64_pact_verifier_cli:" $sha_linux_x86_64_pact_verifier_cli
+
+shas_pact_mock_server_cli=()
+for platform in osx linux; do 
+    for arch in aarch64 x86_64; do 
+
+        echo "⬇️  Downloading $version_pact_mock_server_cli pact_mock_server_cli-${platform}-${arch}.tar.gz from $homepage_pact_reference"
+        curl -LO $homepage_pact_reference/releases/download/pact_mock_server_cli-v$version_pact_mock_server_cli/pact_mock_server_cli-${platform}-${arch}.gz
+
+        brewshasignature=( $(eval "openssl dgst -sha256 pact_mock_server_cli-${platform}-${arch}.gz") )
+        echo "🔏 Checksum SHA256:\t ${brewshasignature[1]} for ${arch}"
+
+        echo "⬇️  Downloading $version_pact_mock_server_cli pact_mock_server_cli-${platform}-${arch}.gz.sha256 for ${platform}-${arch}"
+        echo "curl -LO $homepage_pact_reference/releases/download/pact_mock_server_cli-v$version_pact_mock_server_cli/pact_mock_server_cli-${platform}-${arch}.gz.sha256"
+        curl -LO $homepage_pact_reference/releases/download/pact_mock_server_cli-v$version_pact_mock_server_cli/pact_mock_server_cli-${platform}-${arch}.gz.sha256
+
+        expectedsha=( $(eval "cat pact_mock_server_cli-${platform}-${arch}.gz.sha256") )
+        echo "🔏 Expected SHA1:\t ${expectedsha[0]} for ${platform}-${arch}"
+
+        if [ "${brewshasignature[1]}" == "${expectedsha[0]}" ]; then
+            echo "👮‍♀️ SHA Check: 👍 for ${arch}"
+        else
+            echo "👮‍♀️ SHA Check: 🚨 - checksums do not match! for ${arch}"
+            # exit 1
+        fi
+
+        echo "🧹 Cleaning up..."
+        rm pact_mock_server_cli-${platform}-${arch}.gz
+        rm pact_mock_server_cli-${platform}-${arch}.gz.sha256
+        echo "🔏 Checksum SHA256:\t ${brewshasignature[1]} for ${platform}-${arch}"
+        echo "🧪 Writing formulae..."
+        shas_pact_mock_server_cli+=(${brewshasignature[1]})
+    done 
+done 
+
+    sha_osx_arm64_pact_mock_server_cli=${shas_pact_mock_server_cli[0]}
+    sha_osx_x86_64_pact_mock_server_cli=${shas_pact_mock_server_cli[1]}
+    sha_linux_arm64_pact_mock_server_cli=${shas_pact_mock_server_cli[2]}
+    sha_linux_x86_64_pact_mock_server_cli=${shas_pact_mock_server_cli[3]}
+    echo "sha_osx_arm64_pact_mock_server_cl:" $sha_osx_arm64_pact_mock_server_cli
+    echo "sha_osx_x86_64_pact_mock_server_cl:" $sha_osx_x86_64_pact_mock_server_cli
+    echo "sha_linux_arm64_pact_mock_server_cl:" $sha_linux_arm64_pact_mock_server_cli
+    echo "sha_linux_x86_64_pact_mock_server_cl:" $sha_linux_x86_64_pact_mock_server_cli
 
     write_homebrew_formulae
 
