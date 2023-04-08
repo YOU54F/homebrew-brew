a2a4a2ac00728ce8905152f62c0febfe2cfbc098
Yousaf Nabi <you@saf.dev>
2023-04-08 19:15:42 +0100

    chore(release): Update version to v3.0.0

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,14 +1,14 @@
 class PactRubyStandalone < Formula
   desc "Standalone pact CLI executable using the Ruby Pact impl and Travelling Ruby"
-  homepage "https://github.com/pact-foundation/pact-ruby-standalone"
-  url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.92.0/pact-1.92.0-osx.tar.gz"
-  sha256 "dee18427b9eced63a159d5e64c5ff0d7aa2d4a1e67255b24b239c007c2c8b6c1"
+  homepage "https://github.com/you54f/pact-ruby-standalone"
+  url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.0.0/pact-3.0.0-osx-arm64.tar.gz"
+  sha256 "29caaaa05e8a97d454f8c251bf4642e9999e8e7d5e4b3dc1c74b3ad45f2d13ce"
 
   def install
     bin.install Dir["bin/*"]
     lib.install Dir["lib/*"]
 
-    puts "# Run 'pact-mock-service --help' (see https://github.com/pact-foundation/pact-ruby-standalone/releases/)"
+    puts "# Run 'pact-mock-service --help' (see https://github.com/you54f/pact-ruby-standalone/releases/)"
   end
 
   test do
