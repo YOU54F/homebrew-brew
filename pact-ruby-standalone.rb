2f06a472c1f1ab5d8b04758cbeafe4bf253743dc
Yousaf Nabi <you@saf.dev>
2023-04-08 21:13:26 +0100

    chore(release): Update version to v3.1.0

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -4,23 +4,23 @@ class PactRubyStandalone < Formula
   
   on_macos do
     on_arm do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.0.0/pact-3.0.0-osx-arm64.tar.gz"
-      sha256 "29caaaa05e8a97d454f8c251bf4642e9999e8e7d5e4b3dc1c74b3ad45f2d13ce"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-osx-arm64.tar.gz"
+      sha256 "26675141503abcaf5eb7ed504abd6e3e165631bc2762a315ce54d9b7124a0ff6"
     end
     on_intel do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.0.0/pact-3.0.0-osx-x86_64.tar.gz"
-      sha256 "78c8dcf780a94757a1875067409fe8c0b7b6966bf4b68ff3ee0e86c05ee91293"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-osx-x86_64.tar.gz"
+      sha256 "ba8f2f1504a261b7e6c0879bea3926b67102942efc982dec8255e0cf93f42b94"
     end
   end
   
   on_linux do
     on_arm do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.0.0/pact-3.0.0-linux-arm64.tar.gz"
-      sha256 "adc3ff0df2b9bf80051f17a3a769b94efb2d8be0d4fe30a7fefb5404d579ee2c"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-linux-arm64.tar.gz"
+      sha256 "41b271ca318f17ba90e6014d54226021526066e43ddad5e270e31522ef200720"
     end
     on_intel do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.0.0/pact-3.0.0-linux-x86_64.tar.gz"
-      sha256 "cb97b2fd1f23bc0b4c103db5a6e2d1b5d9d60039af59e93775fcbd3d24d40e70"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-linux-x86_64.tar.gz"
+      sha256 "94befa09a693527c89b357fad0328d088eb076a72b28ba97ab45d05d7c24980d"
     end
   end
 
