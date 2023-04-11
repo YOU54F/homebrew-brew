93c7e1ae9abf247a89250b6a07371c0d710a61a2
YOU54F <YOU54F@users.noreply.github.com>
2023-04-11 21:33:28 +0000

    chore(release): Update version to v3.1.2.1-alpha

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,12 +1,12 @@
 class PactRubyStandalone < Formula
   desc "Standalone pact CLI executable using the Ruby Pact impl and Travelling Ruby"
   homepage "https://github.com/you54f/pact-ruby-standalone"
-  version "3.1.0"
+  version "3.1.2.1-alpha"
   
   on_macos do
     on_arm do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-osx-arm64.tar.gz"
-      sha256 "26675141503abcaf5eb7ed504abd6e3e165631bc2762a315ce54d9b7124a0ff6"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.2.1-alpha/pact-3.1.2.1-alpha-osx-arm64.tar.gz"
+      sha256 "d84030d11ca3c274082850dd431154d739201cb43bcd05950ce7a51ac8897387"
       resource "pact_verifier_cli" do
           url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-osx-aarch64.gz"
           sha256 "e6651e4dab1ad381d9f34fa3a9f12c7756b1bf778d864ab0893fa6078b2a98e8"
@@ -17,8 +17,8 @@ class PactRubyStandalone < Formula
       end
     end
     on_intel do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-osx-x86_64.tar.gz"
-      sha256 "ba8f2f1504a261b7e6c0879bea3926b67102942efc982dec8255e0cf93f42b94"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.2.1-alpha/pact-3.1.2.1-alpha-osx-x86_64.tar.gz"
+      sha256 "005f53146e9fb7a404e206e07b43972e37d1c81d9fb131ef8810d29707e2271f"
       resource "pact_verifier_cli" do
           url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-osx-x86_64.gz"
           sha256 "042ba537784472a65722d5b1301f4d1d61486d7220fbae2691812c44f58350dc"
@@ -32,8 +32,8 @@ class PactRubyStandalone < Formula
   
   on_linux do
     on_arm do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-linux-arm64.tar.gz"
-      sha256 "41b271ca318f17ba90e6014d54226021526066e43ddad5e270e31522ef200720"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.2.1-alpha/pact-3.1.2.1-alpha-linux-arm64.tar.gz"
+      sha256 "dcc73e5fdb8c5489b8e6f49de6221490f57f45b05f99363558f7856b3fb2d52b"
       resource "pact_verifier_cli" do
           url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-linux-aarch64.gz"
           sha256 "48a5d6bbc5471764e77027eaf42f62f812849b083e267be2cb8ed9d36cc862bf"
@@ -44,8 +44,8 @@ class PactRubyStandalone < Formula
       end
     end
     on_intel do
-      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.0/pact-3.1.0-linux-x86_64.tar.gz"
-      sha256 "94befa09a693527c89b357fad0328d088eb076a72b28ba97ab45d05d7c24980d"
+      url "https://github.com/you54f/pact-ruby-standalone/releases/download/v3.1.2.1-alpha/pact-3.1.2.1-alpha-linux-x86_64.tar.gz"
+      sha256 "dcfc20b839467a430278d5ee7c4a31ee3eb80b2d651ac9a9306a636efb53fecd"
       resource "pact_verifier_cli" do
           url "https://github.com/pact-foundation/pact-reference/releases/download/pact_verifier_cli-v0.10.5/pact_verifier_cli-linux-x86_64.gz"
           sha256 "48a5d6bbc5471764e77027eaf42f62f812849b083e267be2cb8ed9d36cc862bf"
