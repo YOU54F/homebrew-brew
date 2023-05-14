f2e1f69f424d4216f7f0583c1998f822c01e2449
Yousaf Nabi <you@saf.dev>
2023-05-14 02:00:36 +0100

    chore: libpact_ffi-v0.4.4

--- Formula/libpact_ffi.rb
+++ Formula/libpact_ffi.rb
@@ -1,23 +1,27 @@
 class LibpactFfi < Formula
   desc "Pact FFI Library for MacOS / Linux"
   homepage "https://github.com/pact-foundation/pact-reference"
-  version "0.0.1"
+  version "0.4.4"
 
   on_macos do
     on_arm do
-      url "https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v0.0.1/libpact_ffi-osx-aarch64-apple-darwin.dylib.gz"
-      sha256 "4856ac43b766f90211bec79c5ffb074e5b1a91bc0a9907434d9a62a0c9535b8e"
+      url "https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v0.4.4/libpact_ffi-osx-aarch64-apple-darwin.dylib.gz"
+      sha256 "d9263847c263b3c31edf67c4322ea566142addbabb45e50449008e893c328b00"
     end
     on_intel do
-      url "https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v0.0.1/libpact_ffi-osx-x86_64.dylib.gz"
-      sha256 "eb557262bcab82a8135f37aff01373eb2a9bedc03a98c9d3bd544916054bbbce"
+      url "https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v0.4.4/libpact_ffi-osx-x86_64.dylib.gz"
+      sha256 "0dcc1c5f42c36dcd38d7f3a37d21baafb050b32a90a0d4215d94a8c99139d939"
     end
   end
 
   on_linux do
+    on_arm do
+      url "https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v0.4.4/libpact_ffi-linux-aarch64.so.gz"
+      sha256 "81a48565b9333dd69851ceebdc71b8cba60c33458e663ceadeddc428f6654f62"
+    end
     on_intel do
-      url "https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v0.0.1/libpact_ffi-linux-x86_64.so.gz"
-      sha256 "5d40f9667664c9c3efe26489f272a24e702718715038d85a1bee5d50098a88ec"
+      url "https://github.com/pact-foundation/pact-reference/releases/download/libpact_ffi-v0.4.4/libpact_ffi-linux-x86_64.so.gz"
+      sha256 "81b4381c52a6da5b479ca031c4f057bc7e24ab91e10b930bab1b09032d836591"
     end
   end
 
